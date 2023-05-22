fa47e7b1a5050a61a34298e474a807caac0fe13a
Eugenie <eugenie.dabrigeon@etu.umontpellier.fr>
2023-05-22 18:48:12 +0200

    hashage mot de passe front

--- controllers/user.js
+++ controllers/user.js
@@ -23,30 +23,31 @@ exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
         .then(user => {
             if (!user) {
-                return res.status(401).json({ message: 'Paire login/mot de passe incorrecte'});
+                return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect' });
             }
-            bcrypt.compare(req.body.password, user.password)
+            bcrypt.compare(req.body.hashedPassword, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ message: 'Paire login/mot de passe incorrecte' });
+                        return res.status(401).json({ message: 'Identifiant ou mot de passe incorrect' });
                     }
                     res.status(200).json({
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
                             'RANDOM_TOKEN_SECRET',
-                            { expiresIn: '24h' }
+                            { expiresIn: '1h' } //mettre 15min
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(500).json({ error: error.message }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({ error: error.message }));
 };
 
+
 exports.deleteUser = (req, res, next) => {
-    User.deleteOne({ _id: req.params.id }).then(
-        () => {
+    User.deleteOne({ _id: req.params.id })
+    .then(() => {
             res.status(200).json({
                 message: 'Deleted!'
             });
