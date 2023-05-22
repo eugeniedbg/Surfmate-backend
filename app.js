b95bbb47d4201ccb708b0ecd87f37ae92a4947ed
Eugenie <eugenie.dabrigeon@etu.umontpellier.fr>
2023-05-22 18:55:43 +0200

    change file name : server.js to index.js

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ mongoose.connect('mongodb+srv://eugenie:1234@cluster0.k2kypnq.mongodb.net/?retry
 
 const app = express();
 
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
